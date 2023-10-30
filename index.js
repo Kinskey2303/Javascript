640a572a8ac2e7f216c3d4cfdad30490b880c17d
Oladipo <oladipotolulope80@gmail.com>
2023-10-30 17:06:16 +0000

    moved code

---
+++ index.js
@@ -0,0 +1,7 @@
+function average(a, b, c, d, e, f, g, h, i, j){
+    return (a+b+c+d+e+f+g+h+i+j)/10;
+    }
+
+    var av= average(823,7482, 54156, 516, 546, 98, 7491, 3541, 8423, 1561);
+
+console.log(av);
